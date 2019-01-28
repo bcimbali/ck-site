30ac9e3344dbd50632f2d4991e8e6f652e57a081
Brett Cimbalik <bcimbali@gmail.com>
2019-01-27 20:04:17 -0600

    Add .prettierrc file to root

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('/Users/BrettCimbalik/code/ck-site/node_modules/gatsby-plugin-offline/gatsby-browser'),
+      options: {"plugins":[]},
+    },{
       plugin: require('/Users/BrettCimbalik/code/ck-site/gatsby-browser.js'),
       options: {"plugins":[]},
     }]
