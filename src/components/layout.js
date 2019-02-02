88dc0324fbd257598abf018e1043a8c65135199e
Brett Cimbalik <bcimbali@gmail.com>
2019-02-02 10:51:15 -0600

    Add favicon to React Helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,8 +4,10 @@ import { StaticQuery, graphql } from 'gatsby';
 
 import Footer from './footer';
 import Header from './header';
+import Helmet from "react-helmet";
 import PropTypes from 'prop-types';
 import React from 'react';
+import favicon from '../images/favicon.ico';
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -24,6 +26,15 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <Helmet 
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: data.site.siteMetadata.description },
+          ]}
+          link={[
+            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+          ]}
+        />
         <Header menuLinks={data.site.siteMetadata.menuLinks} siteTitle={data.site.siteMetadata.title} />
         <div>
           {children}
