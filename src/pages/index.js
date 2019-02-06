5e51077a918f9252335eb5b9b6def7a781a3aa98
Brett Cimbalik <bcimbali@gmail.com>
2019-02-05 20:44:46 -0600

    Add box layout for homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,54 @@ import styled from 'styled-components';
 
 const MainText = styled.h1`
   font-size: 1.75rem;
-  text-align: center
+  margin-bottom: 2rem;
+  text-align: center;
+`;
+
+const PortfolioBox = styled.div`
+  border: 1px solid #FFFFFF;
+  height: 35vh;
+  margin-bottom: 2rem;
+  width: 25vw;
+
+  @media (max-width: 768px) {
+    width: 35vw;
+  }
+
+  @media (max-width: 414px) {
+    width: 100vw;
+  }
+`;
+
+const PortfolioContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 80vw;
+`;
+
+const IndexWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 `;
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`marketing`, `digital marketing`, `chicago`, `content marketing`]} />
-    <MainText>
-      Content + Digital Marketing
-    </MainText>
+    <IndexWrapper>
+      <MainText>
+        Content + Digital Marketing
+      </MainText>
+      <PortfolioContainer>
+        <PortfolioBox>1</PortfolioBox>
+        <PortfolioBox>2</PortfolioBox>
+        <PortfolioBox>3</PortfolioBox>
+        <PortfolioBox>4</PortfolioBox>
+        <PortfolioBox>5</PortfolioBox>
+        <PortfolioBox>6</PortfolioBox>
+      </PortfolioContainer>
+    </IndexWrapper>
   </Layout>
 )
 
