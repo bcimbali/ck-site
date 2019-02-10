73029d906d45aeca9daf013b2d4f37da6bbd8994
Brett Cimbalik <bcimbali@gmail.com>
2019-02-10 16:20:21 -0600

    Add basic layout to portfolio page

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,8 +1,76 @@
 import { Link, StaticQuery, graphql } from 'gatsby';
 
+import Img from 'gatsby-image';
 import Layout from '../components/layout';
 import React from 'react';
 import SEO from '../components/seo';
+import styled from 'styled-components';
+
+const PortfolioImgContainer = styled.div`
+  align-items: center;
+  border: 1px solid #FFFFFF;
+  display: flex;
+  height: 40vh;
+  justify-content: center;
+  /* margin-bottom: 2rem; */
+  overflow: hidden;
+  width: 25vw;
+
+  /* img {
+    flex-shrink: 0;
+    min-width: 100%;
+    min-height: 100%
+  } */
+
+  @media (max-width: 768px) {
+    width: 35vw;
+  }
+
+  @media (max-width: 414px) {
+    width: 100vw;
+  }
+`;
+
+const PortfolioItemRow = styled.div`
+  align-items: center;
+  border: 1px solid #FFFFFF;
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+  
+  :hover {
+    background-color: rgba(255, 255, 255, 0.2);
+    h2 {
+      color: blue;
+    }
+  }
+`;
+
+const PortfolioWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+  width: 80vw;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+const ItemContainer = styled.div`
+  width: 40vw;
+  h2 {
+    color: #FFFFFF;
+    font-size: 1.5rem;
+    text-align: center;
+  }
+`;
+
+const TitleText = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  text-align: center;
+`;
 
 const PORTFOLIO_QUERY = graphql`
   query PortfolioQuery {
@@ -12,6 +80,13 @@ const PORTFOLIO_QUERY = graphql`
           html
           excerpt
           frontmatter {
+            hero {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid_tracedSVG
+                }
+              }
+            }
             title
             slug
           }
@@ -24,23 +99,27 @@ const PORTFOLIO_QUERY = graphql`
 const Portfolio = () => (
   <Layout>
     <SEO title="Portfolio" />
-    <h1>Portfolio</h1>
-    <StaticQuery
-      query={PORTFOLIO_QUERY}
-      render={({allMarkdownRemark}) => (
-        allMarkdownRemark.edges.map(({node}) => (
-          <div key={node.frontmatter.slug}>
-            <Link to={`/portfolio-items${node.frontmatter.slug}`}>
-              <h2>{node.frontmatter.title}</h2>
-            </Link>
-            <p>{node.frontmatter.date}</p>
-            <p>{node.rawMarkdownBody}</p>
-            <Link class="read-more" to={`/portfolio-items${node.frontmatter.slug}`}>Read More</Link>
-          </div>
-        ))
-      )}
-    >
-    </StaticQuery>
+      <PortfolioWrapper>
+        <TitleText>
+          Portfolio
+        </TitleText>
+        <StaticQuery
+          query={PORTFOLIO_QUERY}
+          render={({allMarkdownRemark}) => (
+            allMarkdownRemark.edges.map(({node}) => (
+              <Link to={`/portfolio-items${node.frontmatter.slug}`}>
+                <PortfolioItemRow key={node.frontmatter.slug}>
+                  <PortfolioImgContainer />
+                  <ItemContainer>
+                      <h2>{node.frontmatter.title}</h2>
+                  </ItemContainer>
+                </PortfolioItemRow>
+              </Link>
+            ))
+          )}
+        >
+        </StaticQuery>
+      </PortfolioWrapper>
   </Layout>
 )
 
