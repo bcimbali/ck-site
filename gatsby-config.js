43297de52de9e1fb5146700ca6d2a26af3534566
Brett Cimbalik <bcimbali@gmail.com>
2019-02-18 20:15:39 -0600

    Update plugin-manifest for PWA support

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,11 +55,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Christine Kanownik Portfolio`,
+        short_name: `CK Portfolio`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#B1B0E5`,
+        theme_color: `#B1B0E5`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
