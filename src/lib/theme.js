79545ccc4360f81da068772114e8732889d38351
Brett Cimbalik <bcimbali@gmail.com>
2023-10-03 08:38:31 -0500

    feat(theme): add ThemeProvider & initial them obj

---
+++ src/lib/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+  colors: {
+    bg: '#b1b0e5',
+    black: '#15171c',
+    blue: '#0000FF',
+    fg: '#FFFFFF',
+    green: '#a5dbcb',
+    white: '#FFFFFF',
+  },
+}
+
+export default theme
